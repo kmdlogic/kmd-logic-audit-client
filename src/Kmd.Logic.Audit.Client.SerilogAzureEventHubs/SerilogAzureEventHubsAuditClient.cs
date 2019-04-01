7fb3071a8cd7089170f044cb054f87e6a9732660
Adam Chester <adamchester@gmail.com>
2019-04-01 11:24:08 +1000

    Add `SerilogAzureEventHubs` client implmentation

---
+++ src/Kmd.Logic.Audit.Client.SerilogAzureEventHubs/SerilogAzureEventHubsAuditClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using Serilog;
+using Serilog.Core;
+
+namespace Kmd.Logic.Audit.Client.SerilogAzureEventHubs
+{
+    public class SerilogAzureEventHubsAuditClient : IAudit, IDisposable
+    {
+        private readonly Logger disposableLogger;
+        private readonly SerilogLoggerAudit audit;
+
+        public SerilogAzureEventHubsAuditClient(SerilogAzureEventHubsAuditClientConfiguration config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            var configBuilder = new LoggerConfiguration()
+                .AuditTo.AzureEventHub(
+                    formatter: new Serilog.Formatting.Compact.RenderedCompactJsonFormatter(),
+                    connectionString: config.ConnectionString,
+                    eventHubName: config.AuditEventTopic)
+                ;
+
+            if (config.EnrichFromLogContext == true)
+            {
+                configBuilder = configBuilder.Enrich.FromLogContext();
+            }
+
+            this.disposableLogger = configBuilder.CreateLogger();
+            this.audit = new SerilogLoggerAudit(this.disposableLogger);
+        }
+
+        public void Write(string messageTemplate, params object[] propertyValues)
+        {
+            this.audit.Write(messageTemplate, propertyValues);
+        }
+
+        public IAudit ForContext(string propertyName, object value, bool captureObjectStructure = false)
+        {
+            return this.audit.ForContext(propertyName, value, captureObjectStructure);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.disposableLogger.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/Kmd.Logic.Audit.Client.SerilogAzureEventHubs/SerilogAzureEventHubsAuditClientConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kmd.Logic.Audit.Client.SerilogAzureEventHubs
+{
+    public class SerilogAzureEventHubsAuditClientConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the Azure EventHubs connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets or sets the the topic where audit events are ingested on Azure EventHubs.
+        /// Normally just leave this as the default value of "audit".
+        /// </summary>
+        public string AuditEventTopic { get; set; } = "audit";
+
+        public bool? EnrichFromLogContext { get; set; }
+    }
+}
