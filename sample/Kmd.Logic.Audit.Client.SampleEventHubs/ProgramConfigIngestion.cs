ec254b017a7ef25370b1cb0e09e627cff6456338
Adam Chester (UDV) <udv@kmd.dk>
2019-05-10 16:00:58 +1000

    SampleEventHubs: parallel sending

--- sample/Kmd.Logic.Audit.Client.SampleEventHubs/ProgramConfigIngestion.cs
+++ sample/Kmd.Logic.Audit.Client.SampleEventHubs/ProgramConfigIngestion.cs
@@ -6,6 +6,8 @@ namespace Kmd.Logic.Audit.Client.Sample
     {
         public int NumberOfEventsToSend { get; set; } = 1000;
 
+        public int NumberOfThreads { get; set; } = 1;
+
         public string ConnectionString { get; set; }
 
         public string AuditEventTopic { get; set; } = "audit";
