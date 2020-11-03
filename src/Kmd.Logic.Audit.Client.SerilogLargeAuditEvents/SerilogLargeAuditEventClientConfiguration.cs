7dff721beb4636825f6d824dfebaa420b1f63e04
smahapatra86 <58024185+smahapatra86@users.noreply.github.com>
2020-11-03 14:21:43 +0530

    Custom sink for handling large audit events (#15)
    * added structure for the additional serilog sink for blob and eventhub
    
    * added addition blob storage serilog sink. now audit events would go to both eventhub and blob destinations
    
    * Separate extension logger methods created for blob and eventhub for the purpose of conditional logging
    
    * Adding custom sink
    
    * adding blob upload
    
    * upload blob with name as event id
    
    * Renaming project
    
    * restructured the folders
    
    * adding event hub upload
    
    * Adding message check
    
    * upgrade image to chcek if build is passing
    
    * reverting the image upgrade as build failed
    
    * configurable event size limit and renaming variables
    
    * removing unwanted files
    
    * changing variable name
    
    * audit client
    
    * pr comments
    
    * pr comments
    
    * pr comments
    
    * pr comments
    
    * library updated
    
    * fixing appveyor build issues
    
    * code fix for build failure
    
    * pr review comments
    
    * removing blob name from class variable
    
    * pr comments
    
    * renaming blob name
    
    * removing unused package reference
    
    * remove unwanted interfaces
    
    * refactoring
    
    * pr comments
    
    * renaming variable
    
    * added extension method accepting blob client
    
    * pr comments
    
    * pr comments
    
    Co-authored-by: Avin Mathew (UYW) <UYW@kmd.dk>
    Co-authored-by: Satyajit <YYW@kmd.dk>

---
+++ src/Kmd.Logic.Audit.Client.SerilogLargeAuditEvents/SerilogLargeAuditEventClientConfiguration.cs
@@ -0,0 +1,47 @@
+﻿namespace Kmd.Logic.Audit.Client.SerilogLargeAuditEvents
+{
+    public class SerilogLargeAuditEventClientConfiguration
+    {
+        /// <summary>
+        /// Gets or sets a value that will be tagged on every event to make
+        /// it clear what the source of the event was.
+        /// </summary>
+        public string EventSource { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Azure EventHubs connection string.
+        /// </summary>
+        public string EventHubConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Azure Blob ConnectionString.
+        /// </summary>
+        public string StorageConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Azure Blob account name.
+        /// </summary>
+        public string StorageAccountName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Azure Blob container name.
+        /// </summary>
+        public string StorageContainerName { get; set; } = "logs";
+
+        /// <summary>
+        /// Gets or sets the the topic where audit events are ingested on Azure EventHubs.
+        /// Normally just leave this as the default value of "audit".
+        /// </summary>
+        public string AuditEventTopic { get; set; } = "audit";
+
+        /// <summary>
+        /// Gets or sets if it is going to use enrich from logcontext
+        /// </summary>
+        public bool? EnrichFromLogContext { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event size limit which decides whether message will be pushed to blob and event hub or only event hub
+        /// </summary>
+        public int EventSizeLimitinBytes { get; set; } = 256 * 1024;
+    }
+}
