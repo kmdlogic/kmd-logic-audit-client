170826328757dfb825ec8a4fc7ae3c318a90a34d
smahapatra86 <YYW@kmd.dk>
2020-11-17 10:14:39 +0530

    formatting

--- sample/Kmd.Logic.Audit.Client.SampleLargeAuditEvents/Program.cs
+++ sample/Kmd.Logic.Audit.Client.SampleLargeAuditEvents/Program.cs
@@ -38,7 +38,7 @@ namespace Kmd.Logic.Audit.Client.SampleLargeAuditEvents
 
             // Create data for audit event
             var events = new List<AuditEvent>();
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 10; i++)
             {
                 var data = new AuditEvent();
                 events.Add(data);
@@ -74,7 +74,7 @@ namespace Kmd.Logic.Audit.Client.SampleLargeAuditEvents
                         .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
                         .Select(i =>
                         {
-                            audit.Write("Sample audit event with {IterationNum} from {Application} v{Version} and data {Data}", i, name, version, events);
+                            audit.Write("Sample audit event with {IterationNum} from {Application} v{Version} and data {@Data}", i, name, version, events);
 
                             var numDividedBy10Or1 = config.Ingestion.NumberOfEventsToSend < 10
                                 ? 1
