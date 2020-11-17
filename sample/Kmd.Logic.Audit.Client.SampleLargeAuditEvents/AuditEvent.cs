415c51ee26cb25e95c429a44b950b6103c6d58ba
smahapatra86 <58024185+smahapatra86@users.noreply.github.com>
2020-11-17 16:49:28 +0530

    Sample application to use the implementation of large audit event client  (#24)
    * Adding sample to use large audit event
    
    * pr comments
    
    * formatting
    
    * pr comments
    
    Co-authored-by: smahapatra86 <YYW@kmd.dk>

---
+++ sample/Kmd.Logic.Audit.Client.SampleLargeAuditEvents/AuditEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kmd.Logic.Audit.Client.SampleLargeAuditEvents
+{
+    public class AuditEvent
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid Name { get; set; } = Guid.NewGuid();
+
+        public Guid Department { get; set; } = Guid.NewGuid();
+
+        public Guid Age { get; set; } = Guid.NewGuid();
+    }
+}
---
+++ sample/Kmd.Logic.Audit.Client.SampleLargeAuditEvents/ProgramConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Kmd.Logic.Audit.Client.SampleLargeAuditEvents
+{
+    public class ProgramConfig
+    {
+        public ProgramConfigIngestion Ingestion { get; set; } = new ProgramConfigIngestion();
+
+        public ProgramConfigClient Client { get; set; } = new ProgramConfigClient();
+    }
+}
